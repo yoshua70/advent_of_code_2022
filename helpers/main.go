86e1fb861449b5af7974d9e443a6bd3b5a7f2277
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-04 23:57:01 +0200

    Add helper function to open input file

---
+++ helpers/main.go
@@ -0,0 +1,17 @@
+package helpers
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func GetInput(file_name string) (*os.File, error) {
+	input, err := os.Open(file_name)
+
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Error while opening file %s", file_name))
+	}
+
+	return input, nil
+}
